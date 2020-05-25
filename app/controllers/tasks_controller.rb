e66f93510060304d48b3cbfa93ff880091f52b1e
argyros.white <argyros1231@gmail.com>
2020-05-25 14:06:17 +0000

    sort

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all.page(params[:page])
+    @tasks = Task.order(:id).page(params[:page])
   end
 
   def show
