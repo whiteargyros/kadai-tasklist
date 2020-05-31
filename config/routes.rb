ba5f025f3bfdc68acc6927268f16a87430b00394
argyros.white <argyros1231@gmail.com>
2020-05-31 14:40:28 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
-  
+  root to: 'toppages#index'
+  get 'toppages/index'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'signup', to: 'users#new'
   resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
 end
