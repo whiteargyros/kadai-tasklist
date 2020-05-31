09a02bd0258446c16247ca1af690e9b99d81b4f5
argyros.white <argyros1231@gmail.com>
2020-05-31 15:10:30 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get 'toppages/index'
   get 'users/index'
   get 'users/show'
